4635143d3fbe008ca368e4c0d217c2eee979827d
Alan Renner <renner.131@osu.edu>
2020-08-28 22:45:16 -0700

    Final Server

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/nucampsite",
+};
+
+module.exports = {
+  secretKey: "12345-67890-09876-54321",
+  mongoUrl: "mongodb://localhost:27017/nucampsite",
+  facebook: {
+    clientId: "311044733298901",
+    clientSecret: "7c4f0bb6830386aaf43d51bc73e8c049",
+  },
+};
